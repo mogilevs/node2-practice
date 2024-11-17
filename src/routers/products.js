0a210f338f430b4af69699e3c9ca4a1255ea17c7
Yuliia Vorobei <vorobeju1@gmail.com>
2024-11-17 14:50:18 +0100

    task 3

--- src/routers/products.js
+++ src/routers/products.js
@@ -16,27 +16,19 @@ import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
-router.get('/products', ctrlWrapper(getAllProductsController));
-router.get(
-  '/products/:productId',
-  isValidId,
-  ctrlWrapper(getProductByIdController),
-);
+router.get('/', ctrlWrapper(getAllProductsController));
+router.get('/:productId', isValidId, ctrlWrapper(getProductByIdController));
 router.post(
-  '/products',
+  '/',
   validateBody(createProductsSchema),
   ctrlWrapper(createProductController),
 );
 router.patch(
-  '/products/:productId',
+  '/:productId',
   isValidId,
   validateBody(updateProductsSchema),
   ctrlWrapper(updateProductController),
 );
-router.delete(
-  '/products/:productId',
-  isValidId,
-  ctrlWrapper(deleteProductController),
-);
+router.delete('/:productId', isValidId, ctrlWrapper(deleteProductController));
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import productsRouter from './routers/products.js';
+import authRouter from './routers/auth.js';
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
@@ -13,7 +14,8 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
 
-  app.use(productsRouter);
+  app.use('/users', authRouter);
+  app.use('/products', productsRouter);
 
   app.use('*', notFoundHandler);
 
